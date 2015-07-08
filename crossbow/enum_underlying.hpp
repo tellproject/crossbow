e07c3a0e28339a3bf5c382a4ed7d6476bfa07e38
Kevin Bocksrocker <bokevin@student.ethz.ch>
2015-07-08 17:41:10 +0200

    Move the underlying enum helpers from tellstore into crossbow

---
+++ crossbow/enum_underlying.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <type_traits>
+
+namespace crossbow {
+
+template<typename Enum>
+constexpr auto to_underlying(Enum e) -> typename std::underlying_type<Enum>::type {
+    return static_cast<typename std::underlying_type<Enum>::type>(e);
+}
+
+template<typename Enum>
+constexpr Enum from_underlying(typename std::underlying_type<Enum>::type s) {
+    return static_cast<Enum>(s);
+}
+
+} // namespace crossbow
